763ebb66e23d8d14c472acdc3aeda00506c2ce02
Tobias Dustin <tobydustin@outlook.com>
2018-02-22 15:17:56 +0000

    Editted Ticket Script

--- Customer_Dashboard.php
+++ Customer_Dashboard.php
@@ -31,7 +31,7 @@ require 'connection.php';
           if (isset($_GET["address"]))
           {
               $a = $_GET["address"];
-              $result = $conn->query("SELECT * FROM tbl_ticket WHERE address = '$a' ");
+              $result = $conn->query("SELECT first_name,last_name,service_name,addressLine1 FROM tbl_ticket t JOIN tbl_clients c ON (t.client_id=c.client_id) JOIN tbl_services s ON (s.serv_id = t.service_id) JOIN tbl_location l ON (t.loc_id=l.loc_id) WHERE l.loc_id=$a");
 
               $row = $result->fetch();
               if($row == false){
@@ -41,10 +41,10 @@ require 'connection.php';
                   while($row != false){
                       echo '<div class = "tickets">';
                       echo "<p>";
-                      echo "First Name: $row[firstName]<br>";
-                      echo "Second Name: $row[lastName]<br>";
-                      echo "Service: $row[service]<br>";
-                      echo "Address: $row[address]<br>";
+                      echo "First Name: $row[first_name]<br>";
+                      echo "Second Name: $row[last_name]<br>";
+                      echo "Service: $row[service_name]<br>";
+                      echo "Address: $row[addressLine1]<br>";
                       echo "</p>";
                       echo '</div>';
                       $row=$result->fetch();
